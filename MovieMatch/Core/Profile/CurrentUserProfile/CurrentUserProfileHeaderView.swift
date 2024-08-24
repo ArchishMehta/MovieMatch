5619264155d601da5fb9322d00a3a7e7062a2ecc
Archish Mehta <archie@Archishs-MacBook-Air.local>
2024-08-24 14:46:30 -0400

    small tweaks to profile page

--- MovieMatch/Core/Profile/CurrentUserProfile/CurrentUserProfileHeaderView.swift
+++ MovieMatch/Core/Profile/CurrentUserProfile/CurrentUserProfileHeaderView.swift
@@ -3,43 +3,79 @@
 //  MovieMatch
 //
 //  Created by Archish Mehta on 2024-08-21.
-//
-
 import SwiftUI
 
 struct CurrentUserProfileHeaderView: View {
     let user: User
+    @State private var isImagePickerPresented = false
+    @State private var selectedImage: UIImage?
+    @State private var profileImageURL: String?
+
     var body: some View {
         VStack {
-            ZStack(alignment: .topTrailing) {
-                Image(user.profileImageURLs[0])
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 120, height:120 )
-                    .clipShape(Circle())
+            ZStack(alignment: .bottomTrailing) {
+                Button(action: {
+                    isImagePickerPresented = true
+                }) {
+                    Image(uiImage: selectedImage ?? UIImage(named: user.profileImageURLs.first ?? "defaultProfileImage")!)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 140, height: 140)
+                        .clipShape(Circle())
+                        .background {
+                            Circle()
+                                .fill(Color(.systemGray6))
+                                .frame(width: 148, height: 148)
+                                .shadow(radius: 10)
+                        }
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 4)
+                        )
+                }
+
+                Image(systemName: "pencil.circle.fill")
+                    .foregroundStyle(Color.blue)
                     .background {
                         Circle()
-                            .fill(Color(.systemGray6))
-                            .frame(width: 128, height: 128)
-                            .shadow(radius: 10)
+                            .fill(Color.white)
+                            .frame(width: 36, height: 36)
                     }
-                
-                Image(systemName: "pencil")
-                    .imageScale(.small)
-                    .foregroundStyle(.black)
-                    .background {
-                        Circle()
-                            .fill(.white)
-                            .frame(width: 32, height: 32)
-                    }
-                    .offset(x: -8, y: 10)
+                    .offset(x: -10, y: -10)
             }
             Text("\(user.movie), \(user.year)")
                 .font(.title2)
                 .fontWeight(.light)
+                .padding(.top, 8)
         }
+        .padding(.top, 20)
         .frame(maxWidth: .infinity)
-        .frame(height: 240)
+        .frame(height: 260)
+        .sheet(isPresented: $isImagePickerPresented) {
+            ImagePicker(selectedImage: $selectedImage)
+                .onChange(of: selectedImage) { oldValue, newValue in
+                    if let newValue = newValue {
+                        saveImage(newValue)
+                    }
+                }
+        }
+    }
+
+    func saveImage(_ image: UIImage) {
+        // Convert UIImage to data
+        if let imageData = image.jpegData(compressionQuality: 0.8) {
+            // Save the image data locally or upload it to a server and get the URL
+            let fileName = UUID().uuidString + ".jpg"
+            let fileURL = FileManager.default.temporaryDirectory.appendingPathComponent(fileName)
+            
+            do {
+                try imageData.write(to: fileURL)
+                profileImageURL = fileURL.absoluteString
+                print("DEBUG: Image saved at \(fileURL.absoluteString)")
+            } catch {
+                print("Error saving image: \(error)")
+            }
+        }
     }
 }
 
--- MovieMatch/Core/Profile/EditProfile/ProfileImageGridView.swift
+++ MovieMatch/Core/Profile/EditProfile/ProfileImageGridView.swift
@@ -1,43 +1,59 @@
-//
-//  ProfileImageGridView.swift
-//  MovieMatch
-//
-//  Created by Archish Mehta on 2024-08-21.
-//
-
 import SwiftUI
 
 struct ProfileImageGridView: View {
     let user: User
-    
+    @State private var isImagePickerPresented = false
+    @State private var selectedImage: UIImage?
+    @State private var selectedImageIndex: Int?
+
     var body: some View {
         LazyVGrid(columns: columns, spacing: 16) {
             ForEach(0..<6, id: \.self) { index in
                 if index < user.profileImageURLs.count {
-                    Image(user.profileImageURLs[index])
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: imageWidth, height: imageHeight)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                } else {
-                    ZStack(alignment: .bottomTrailing) {
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color(.secondarySystemBackground))
+                    Button(action: {
+                        selectedImageIndex = index
+                        isImagePickerPresented = true
+                    }) {
+                        Image(user.profileImageURLs[index])
+                            .resizable()
+                            .scaledToFill()
                             .frame(width: imageWidth, height: imageHeight)
-                        
-                        Image(systemName: "plus.circle.fill")
-                            .imageScale(.large)
-                            .foregroundStyle(Color(.secondaryLabel))
-                            .offset(x: 4, y: 4)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                } else {
+                    Button(action: {
+                        selectedImageIndex = index
+                        isImagePickerPresented = true
+                    }) {
+                        ZStack(alignment: .bottomTrailing) {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color(.secondarySystemBackground))
+                                .frame(width: imageWidth, height: imageHeight)
+                            
+                            Image(systemName: "plus.circle.fill")
+                                .imageScale(.large)
+                                .foregroundStyle(Color(.secondaryLabel))
+                                .offset(x: 4, y: 4)
+                        }
                     }
                 }
             }
         }
+        .sheet(isPresented: $isImagePickerPresented, onDismiss: handleImageSelection) {
+            ImagePicker(selectedImage: $selectedImage)
+        }
+    }
+
+    private func handleImageSelection() {
+        guard let selectedImage = selectedImage, let index = selectedImageIndex else { return }
+        
+        // Handle the logic for saving the image and updating the user's profileImageURLs array here
+        print("DEBUG: Image selected for index \(index)")
     }
 }
 
 private extension ProfileImageGridView {
-    var columns: [GridItem] { 
+    var columns: [GridItem] {
         [
             .init(.flexible()),
             .init(.flexible()),
